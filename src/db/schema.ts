0acc295cfe88259c32df0e8834fe0b4b39101808
ChenHaoJie9527 <a18520145493@163.com>
2024-05-25 18:25:45 +0800

    feat: 创建user表

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,10 @@
-import { pgTable, serial } from 'drizzle-orm/pg-core'
+import { pgTable, serial, text } from 'drizzle-orm/pg-core'
 
 export const bids = pgTable('bb_bids', {
     id: serial('id').primaryKey(),
+})
+
+export const user = pgTable('users', {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
 })
